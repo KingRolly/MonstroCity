6b3dbb7cba4d5cae970ac554e31c7c26d21aad96
Jack Peters <kingrolly2006@gmail.com>
2026-01-27 14:32:03 -0800

    Added fields to TowerTile and created Attack class

--- Assets/Scripts/TowerTile.cs
+++ Assets/Scripts/TowerTile.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class TowerTile : Tile
 {
+    /*
+        All towers are implemented with a modular system. Each possible attribute for a tower's stats and attacks will be hardcoded as options. These will be represented as serialized fields in the editor so that each tower's properties can be edited without code.
+    */
 
+    [SerializeField] int attackMode;
+    /*
+        0: loop
+        1: random
+        2: group random
+    */
+    // attacks: a list of attacks, with type (projectile, AOE, etc.), cooldown, damage and other effects
+    [SerializeField] List<Attack> attacks = new List<Attack>();
+
+    protected override void Start()
+    {
+
+    }
 }
